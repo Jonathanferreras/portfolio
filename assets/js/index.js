0b06a341bf35a6a75d8ce168186c4a13e41ac43c
jonathanferreras <jonathanferreras@icloud.com>
2020-01-14 14:14:59 -0500

    added more styling and content to design

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,12 @@
-console.log('Hello world!');
+window.onload = function() {
+  calcYearsWorking();
+}
+
+
+function calcYearsWorking() {
+  const startDate = new Date("10/05/2017");
+  const currentDate = new Date();
+  const diff = ((currentDate.getTime() - startDate.getTime()) / 1000) / (60 * 60 * 24 * 7 * 4);
+
+  document.getElementById('years-working').innerHTML = Math.floor(diff / 12);
+}
